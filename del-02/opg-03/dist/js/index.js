8a693830d98f83cfb60ae6f9b29dbd760bd15567
Mikkel Back Koll <217236@RTS-365.dk>
2019-04-24 08:50:22 +0200

    Ryddede lidt op i koden og tilføjede en kommentar

--- del-02/opg-03/dist/js/index.js
+++ del-02/opg-03/dist/js/index.js
@@ -1,14 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-	let myButtonElements = document.querySelectorAll('.myButton');
+	let myButtonElements = document.querySelectorAll('#color-buttons button');
 	let rootElement = document.documentElement;
 
 	myButtonElements.forEach((myButtonElement)=>{
 
 		myButtonElement.addEventListener('click',(event)=>{
 			
+			// ComputedStyle er når et givent element har fået sin style fra en style fil, istedet for gennem javascript som jo tilføjer Inline Styling.
 			let collectedColor = getComputedStyle(event.target).getPropertyValue("background-color");
 
+			console.log(collectedColor);
 
 			rootElement.style.setProperty("--chosen-color",collectedColor);
 
@@ -23,4 +25,4 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	
 
-})
+}) // DOMContentLoaded slutter
