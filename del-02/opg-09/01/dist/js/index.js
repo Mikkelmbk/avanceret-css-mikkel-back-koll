fcc1206094faf1578d5fb885c76a37af1bcce831
Mikkel Back Koll <217236@RTS-365.dk>
2019-04-29 13:29:09 +0200

    fade in and fade out opgave 9

--- del-02/opg-09/01/dist/js/index.js
+++ del-02/opg-09/01/dist/js/index.js
@@ -1,5 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+	let btnElements = document.querySelectorAll('button');
+	let contentElements = document.querySelectorAll('.content');
+	let buttonIndex = 0;
+	updateContent(buttonIndex);
 
+	btnElements.forEach((btnElement, index)=>{
+		btnElement.addEventListener('click',()=>{
+			buttonIndex = index;
+			updateContent(buttonIndex);
+		})
+	})
 
+	
+	function updateContent(btnIndex){
+		contentElements.forEach((contentElement, index)=>{
+		contentElement.style.opacity = 0;
+		contentElement.style.pointerEvents = "none";
+		// contentElement.style.display = "none";
+	
+		if(btnIndex == index){
+			// contentElement.style.display = "block";
+			contentElement.style.opacity = 1;
+			contentElement.style.pointerEvents = "auto";
+			
+
+		}
+	})
+
+	}
+
+	
 }) // DOMContentLoaded slutter
