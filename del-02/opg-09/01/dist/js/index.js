371baca0964e8a243a25c0d75eebe5bb8cd176d0
Mikkel Back Koll <217236@RTS-365.dk>
2019-04-30 12:53:43 +0200

    Footeren tilpasser sig dynamisk afhængigt af højden på min position absolute elementer

--- del-02/opg-09/01/dist/js/index.js
+++ del-02/opg-09/01/dist/js/index.js
@@ -3,6 +3,9 @@ document.addEventListener('DOMContentLoaded', () => {
 	let btnElements = document.querySelectorAll('button');
 	let contentElements = document.querySelectorAll('.content');
 	let buttonIndex = 0;
+	let topDistance;
+	let elementHeight;
+	let footerElement = document.querySelector('.footer');
 	updateContent(buttonIndex);
 
 	btnElements.forEach((btnElement, index)=>{
@@ -12,19 +15,28 @@ document.addEventListener('DOMContentLoaded', () => {
 		})
 	})
 
+	window.addEventListener('resize',()=>{
+		updateContent(buttonIndex);
+	})
+
 	
 	function updateContent(btnIndex){
 		contentElements.forEach((contentElement, index)=>{
 		contentElement.style.opacity = 0;
 		contentElement.style.pointerEvents = "none";
-		// contentElement.style.display = "none";
-	
+		contentElement.style.height = "0px";
+		contentElement.style.width = "0px";
+		
+		
 		if(btnIndex == index){
-			// contentElement.style.display = "block";
 			contentElement.style.opacity = 1;
 			contentElement.style.pointerEvents = "auto";
-			
+			contentElement.style.width = "auto";
+			contentElement.style.height = "auto";
 
+			topDistance = contentElement.offsetTop;
+			elementHeight = contentElement.offsetHeight;
+			footerElement.style.marginTop = topDistance + elementHeight + "px";
 		}
 	})
 
